15cb231bd6089b94e8d05c98e9a53671a8d05144
onizuka <aleksa.curcic.dev@gmail.com>
2025-12-17 19:38:04 +0100

    feature: add nav-bar base component

--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavBarBase} from './shared/components/nav-bar/nav-bar-base/nav-bar-base';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarBase],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
