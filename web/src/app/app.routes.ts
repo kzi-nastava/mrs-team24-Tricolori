edd38ebf62c12a3f9bf0b12836a315804e1396ae
onizuka <aleksa.curcic.dev@gmail.com>
2025-12-18 21:03:41 +0100

    feature: reset password page

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import {Login} from './features/auth/login/login';
 import {Register} from './features/auth/register/register';
+import {ResetPassword} from './features/auth/reset-password/reset-password';
 
 export const routes: Routes = [
   {path: "login", component: Login},
   {path: "register", component: Register},
-  {path: "driver-history", loadComponent: () => import('./features/ride-history/driver-history').then(m => m.DriverHistory)},
+  {path: "driver-history", loadComponent: () =>
+      import('./features/ride-history/driver-history').then(m => m.DriverHistory)},
+  {path: "reset-password", component: ResetPassword}
 ];
