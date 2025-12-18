931be5c6ebf1c69441d5a9e630f5806ab713355d
Duja03 <mdujanovic03@gmail.com>
2025-12-18 20:05:36 +0100

    feature: create form for changing the password

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import {Login} from './features/auth/login/login';
 import {Register} from './features/auth/register/register';
+import { Profile } from './features/profile/profile';
+import { PasswordChange } from './features/password-change/password-change';
 
 export const routes: Routes = [
   {path: "login", component: Login},
-  {path: "register", component: Register}
+  {path: "register", component: Register},
+  {path: "profile", component: Profile},
+  {path: "password-change", component: PasswordChange}
 ];
