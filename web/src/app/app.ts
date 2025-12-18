dd28d10883bf2dabe8e60c68f14624603f91565b
Duja03 <mdujanovic03@gmail.com>
2025-12-18 13:30:58 +0100

    fix: make background white and add shadows

--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavBarBase} from './shared/components/nav-bar/nav-bar-base/nav-bar-base';
+import { Profile } from './features/profile/profile';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBarBase],
+  imports: [RouterOutlet, NavBarBase, Profile],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
