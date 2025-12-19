f527c79d51a4f4840d81d6d17a7bf3a2416c5984
sara-stojkov <sarastojkov6@gmail.com>
2025-12-19 15:22:12 +0100

    fix: remove lazy loading of home page

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -5,16 +5,15 @@ import { ResetPassword } from './features/auth/reset-password/reset-password';
 import { Profile } from './features/profile/profile';
 import { PasswordChange } from './features/password-change/password-change';
 import { UnregisteredHome } from './features/home/unregistered/unregistered-home';
+import { DriverHistory } from './features/ride-history/driver-history';
 
 export const routes: Routes = [
-   {
-    path: '',
-    component: UnregisteredHome},
+  {path: '',component: UnregisteredHome},
   {path: "login", component: Login},
   {path: "register", component: Register},
   {path: "reset-password", component: ResetPassword},
   {path: "profile", component: Profile},
   {path: "password-change", component: PasswordChange},
-  {path: "driver-history", loadComponent: () => import('./features/ride-history/driver-history').then(m => m.DriverHistory)},
-  {path: "unregistered-home", loadComponent: () => import('./features/home/unregistered/unregistered-home').then(m => m.UnregisteredHome)}
+  {path: "driver-history", component: DriverHistory},
+  {path: "unregistered-home", component: UnregisteredHome}
 ];
