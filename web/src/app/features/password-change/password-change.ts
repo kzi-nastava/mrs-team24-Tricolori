a8900192925d687bcb284c822c8851330c848b77
Duja03 <mdujanovic03@gmail.com>
2025-12-23 22:11:38 +0100

    fix: equalize change password with other inputs

--- web/src/app/features/password-change/password-change.ts
+++ web/src/app/features/password-change/password-change.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NgIcon } from '@ng-icons/core';
 
 @Component({
   selector: 'app-password-change',
-  imports: [],
+  imports: [NgIcon],
   templateUrl: './password-change.html',
   styleUrl: './password-change.css',
 })
@@ -11,4 +12,24 @@ export class PasswordChange {
   showCurrentPassword = false;
   showNewPassword = false;
   showConfirmedPassword = false;
+
+  currentPassword: string = '';
+  newPassword: string = '';
+  confirmPassword: string = '';
+
+  isValidLength(): boolean {
+    return this.newPassword.length >= 8;
+  }
+
+  hasUpperAndLowerCase(): boolean {
+    return /[a-z]/.test(this.newPassword) && /[A-Z]/.test(this.newPassword);
+  }
+
+  hasNumber(): boolean {
+    return /\d/.test(this.newPassword);
+  }
+
+  hasSpecialCharacter(): boolean {
+    return /[!@#$%^&*(),.?":{}|<>]/.test(this.newPassword);
+  }
 }
