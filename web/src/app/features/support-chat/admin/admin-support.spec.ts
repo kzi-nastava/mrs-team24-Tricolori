0bb7a12f4883d4ebdb491dea57612fe9456c908a
sara-stojkov <sarastojkov6@gmail.com>
2025-12-24 12:05:45 +0100

    feat: add mock support pages

---
+++ web/src/app/features/support-chat/admin/admin-support.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AdminSupport } from './admin-support';
+
+describe('AdminSupport', () => {
+  let component: AdminSupport;
+  let fixture: ComponentFixture<AdminSupport>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AdminSupport]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(AdminSupport);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
