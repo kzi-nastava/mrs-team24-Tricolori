e2c355a280f98268c2a9dd03142ddf5bb6df3bc7
onizuka <aleksa.curcic.dev@gmail.com>
2025-12-26 01:05:45 +0100

    feature: add ride cancel endpoint

--- backend/src/main/java/com/tricolori/backend/infrastructure/presentation/controllers/RideController.java
+++ backend/src/main/java/com/tricolori/backend/infrastructure/presentation/controllers/RideController.java
@@ -1,14 +1,12 @@
 package com.tricolori.backend.infrastructure.presentation.controllers;
 
+import com.tricolori.backend.infrastructure.presentation.dtos.CancelRideRequest;
 import com.tricolori.backend.infrastructure.presentation.dtos.RideEstimationRequest;
 import com.tricolori.backend.infrastructure.presentation.dtos.RideEstimationResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/rides")
@@ -17,6 +15,12 @@ public class RideController {
 
     @PostMapping("/estimate")
     public ResponseEntity<RideEstimationResponse> estimateRide(@Valid @RequestBody RideEstimationRequest request) {
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}/cancel")
+    public ResponseEntity<Void> cancelRide(@Valid @RequestBody CancelRideRequest request, @PathVariable Long id) {
 
         return ResponseEntity.ok().build();
     }
