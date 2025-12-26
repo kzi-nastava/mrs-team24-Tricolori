8763ea67e2221db68b7ccab4d7c8b8816f362d55
onizuka <aleksa.curcic.dev@gmail.com>
2025-12-26 19:55:59 +0100

    feature: add admin ride history view endpoint

--- backend/src/main/java/com/tricolori/backend/infrastructure/presentation/controllers/RideController.java
+++ backend/src/main/java/com/tricolori/backend/infrastructure/presentation/controllers/RideController.java
@@ -1,10 +1,10 @@
 package com.tricolori.backend.infrastructure.presentation.controllers;
 
-import com.tricolori.backend.infrastructure.presentation.dtos.CancelRideRequest;
-import com.tricolori.backend.infrastructure.presentation.dtos.RideEstimationRequest;
-import com.tricolori.backend.infrastructure.presentation.dtos.RideEstimationResponse;
+import com.tricolori.backend.infrastructure.presentation.dtos.*;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,4 +24,15 @@ public class RideController {
 
         return ResponseEntity.ok().build();
     }
+
+    @PostMapping("/history")
+    public ResponseEntity<Page<RideHistoryResponse>> getAdminRideHistory(
+            @RequestBody RideHistoryFilter filter,
+            Pageable pageable
+    ) {
+
+        return ResponseEntity.ok(Page.empty());
+    }
+
+
 }
