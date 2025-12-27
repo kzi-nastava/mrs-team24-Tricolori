0fe46a139777eda208ffa175870174b823bbe8c7
onizuka <aleksa.curcic.dev@gmail.com>
2025-12-27 11:54:37 +0100

    fix: merge conflicts in stop ride request

--- backend/src/main/java/com/tricolori/backend/infrastructure/presentation/dtos/StopRideRequest.java
+++ backend/src/main/java/com/tricolori/backend/infrastructure/presentation/dtos/StopRideRequest.java
@@ -1,8 +1,9 @@
 package com.tricolori.backend.infrastructure.presentation.dtos;
 
+import com.tricolori.backend.core.domain.models.Address;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
 public record StopRideRequest(
-        @NotNull @Valid LocationDto location
+        @NotNull @Valid Address address
 ) {}
