8960046b6c828eeb95359e2780e89143f0d6f27e
Duja03 <mdujanovic03@gmail.com>
2026-01-10 18:13:26 +0100

    feature: add forms and switch between steps

--- web/src/app/features/auth/driver-register/driver-register.ts
+++ web/src/app/features/auth/driver-register/driver-register.ts
@@ -1,18 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, model } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgIcon } from '@ng-icons/core';
 
 @Component({
   selector: 'app-driver-register',
-  imports: [],
+  imports: [
+    FormsModule, 
+    ReactiveFormsModule,
+    NgIcon
+  ],
   templateUrl: './driver-register.html',
   styleUrl: './driver-register.css',
 })
 
 export class DriverRegister {
-  step = 1;
+  step: number;
+  firstStepForm: FormGroup;
+  secondStepForm: FormGroup;
 
-  nextStep() {
-    // TODO: validate if all fields are filled...
+  selectedStepClasses = "w-10 h-10 bg-base-700 text-white rounded-full flex items-center justify-center font-bold step-shadow";
+  selectedTextClasses = "text-xs mt-2 text-base-600 font-medium";
+  otherStepClasses = "w-10 h-10 border-2 border-gray-200 text-gray-400 rounded-full flex items-center justify-center font-bold";
+  otherTextClasses = "text-xs mt-2 text-gray-400 font-medium";
+
+  constructor() {
+    this.step = 1;
+    this.firstStepForm = new FormGroup({
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+      phone: new FormControl(''),
+      address: new FormControl('')
+    });
+    this.secondStepForm = new FormGroup({
+      email: new FormControl(''),
+      vehicleModel: new FormControl(''),
+      vehicleType: new FormControl(''),
+      registrationPlate: new FormControl(),
+      seatNumber: new FormControl(''),
+      petFriendly: new FormControl(''),
+      babyFriendly: new FormControl(''),
+    });
+  }
+
+  firstSubmit() {
     this.step = 2;
+  }
+
+  secondSubmit() {
+    this.step = 1;
   }
 
   prevStep() {
