f5d558e876e9cc1050587a0bfb7efc76bb19ac4b
Duja03 <mdujanovic03@gmail.com>
2026-01-24 20:51:30 +0100

    fix: reset to step one after submit

--- web/src/app/features/auth/driver-register/driver-register.ts
+++ web/src/app/features/auth/driver-register/driver-register.ts
@@ -1,4 +1,4 @@
-import { Component, inject, model } from '@angular/core';
+import { Component, inject, model, signal } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NgIcon } from '@ng-icons/core';
 import { AdminDriverRegistrationRequest, StepOneDriverRegistrationData, StepTwoDriverRegistrationData } from '../../../shared/model/driver-registration';
@@ -23,7 +23,7 @@ import { AuthService } from '../../../core/services/auth.service';
 export class DriverRegister {
   private authService = inject(AuthService);
 
-  step: number;
+  step = signal(1);
   
   savedStepOneData?: StepOneDriverRegistrationData;
   savedStepTwoData?: StepTwoDriverRegistrationData;
@@ -34,13 +34,9 @@ export class DriverRegister {
   otherStepClasses = "w-10 h-10 border-2 border-gray-200 text-gray-400 rounded-full flex items-center justify-center font-bold";
   otherTextClasses = "text-xs mt-2 text-gray-400 font-medium";
 
-  constructor() {
-    this.step = 1;
-  }
-
   handleStepOne(data: StepOneDriverRegistrationData) {
     this.savedStepOneData = data;
-    this.step = 2;
+    this.step.set(2);
   }
 
   handleStepTwo(data: StepTwoDriverRegistrationData) {
@@ -55,23 +51,25 @@ export class DriverRegister {
     };
 
     const fileToUpload = pfpFile || null;
-
     // Send request:
-    this.authService.registerDriver(finalRequest, fileToUpload).subscribe({
+    /*this.authService.registerDriver(finalRequest, fileToUpload).subscribe({
       next: (res) => { 
         console.log("Success!", res);
-        // Because I use @if, components will be recreated, but this time
-        // since saved data is undefined, they will be brand new, empty...
-        this.savedStepOneData = undefined;
-        this.savedStepTwoData = undefined;
-        this.step = 1;
+        // TODO: add toast to signalize
       },
       error: (err) => { console.log("Error: ", err) }
-    })
+    })*/
+
+    // Because I use @if, components will be recreated, but this time
+    // since saved data is undefined, they will be brand new, empty...
+    this.savedStepOneData = undefined;
+    this.savedStepTwoData = undefined;
+    this.step.set(1);
+
   }
 
   prevStep(data: StepTwoDriverRegistrationData) {
     this.savedStepTwoData = data;
-    this.step = 1;
+    this.step.set(1);
   }
 }
