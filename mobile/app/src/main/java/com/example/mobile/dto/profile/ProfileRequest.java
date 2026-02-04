55f6a65943ed8e82397eb296fc7978f1237732e0
Duja03 <mdujanovic03@gmail.com>
2026-02-04 13:58:35 +0100

    feature: create dtos that match backend and service to target REST endpoint

---
+++ mobile/app/src/main/java/com/example/mobile/dto/profile/ProfileRequest.java
@@ -0,0 +1,62 @@
+package com.example.mobile.dto.profile;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class ProfileRequest {
+    @SerializedName("firstName")
+    @Expose
+    private String firstName;
+
+    @SerializedName("lastName")
+    @Expose
+    private String lastName;
+
+    @SerializedName("homeAddress")
+    @Expose
+    private String homeAddress;
+
+    @SerializedName("phoneNumber")
+    @Expose
+    private String phoneNumber;
+
+    @SerializedName("pfp")
+    @Expose
+    private String pfp;
+
+    /*--- Getters & Setters ---*/
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getHomeAddress() {
+        return homeAddress;
+    }
+    public void setHomeAddress(String homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPfp() {
+        return pfp;
+    }
+    public void setPfp(String pfp) {
+        this.pfp = pfp;
+    }
+}
